31d228606f2bad88a8a0e0c65247f39adcbd9783
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-11-02 19:27:29 +0100

    Create next app with components from pgp-next-basics repo

---
+++ components/Product.js
@@ -0,0 +1,20 @@
+import styles from "../styles/Product.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Product({ product }) {
+  return (
+    <Link href={`/products/${product.id}`}>
+      <div className={styles.container}>
+        <div className={styles.imageContainer}>
+          <img className={styles.image} src={product.image} />
+        </div>
+        <div className={styles.contentContainer}>
+          <a className={styles.productTitle}>{product.title}</a>
+          <a className={styles.productCategory}> {product.category}</a>
+          <a className={styles.productPrice}>$ {product.price}</a>
+        </div>
+      </div>
+    </Link>
+  );
+}
