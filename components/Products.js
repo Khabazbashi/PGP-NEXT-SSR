08c7460915bf6c4ed2607cd737b4fbce7981a22b
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-11-03 14:43:48 +0100

    Show no product if no products found

--- components/Products.js
+++ components/Products.js
@@ -2,11 +2,21 @@ import Product from "./Product";
 import styles from "../styles/Products.module.css";
 
 export default function Products({ products }) {
-  return (
-    <div className={styles.container}>
-      {products.map((product, i) => {
-        return <Product key={i} product={product} />;
-      })}
-    </div>
-  );
+  if (products.length !== 0) {
+    return (
+      <div className={styles.container}>
+        {products.map((product, i) => {
+          return <Product key={i} product={product} />;
+        })}
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.container}>
+        <div className={styles.noProductContainer}>
+          <p className={styles.noProductText}>No products where found. </p>
+        </div>
+      </div>
+    );
+  }
 }
